7a9c2bbcfff31a0d99d6881097d814d29df966a0
DW31 TALDEA <ik012982i9@H012982.NET>
2023-10-23 12:27:33 +0200

    actividades añadidas

--- src/main.java
+++ src/main.java
@@ -1,3 +1,18 @@
+//Ver el string es palindromo
+//ver si el numero es primo
+//ver cuantas mayusculas tiene la cadena
+//si no es primo, factorizalo
+//mostrar el tamaño de la cadena
 public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    //Ver el string es palindromo
+
+    //ver si el numero es primo
+
+    //ver cuantas mayusculas tiene la cadena
+
+    //si no es primo, factorizalo
+
+    //mostrar el tamaño de la cadena
 
         }
