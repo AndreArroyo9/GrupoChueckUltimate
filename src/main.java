8da4f58229a95412128b8af388a96ad277a4aaa2
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-23 13:37:55 +0200

    programa primo

--- src/main.java
+++ src/main.java
@@ -18,13 +18,38 @@ public class main{
 
         System.out.println("Introduce un número: ");
         numero = sc.nextInt();
+
         //Ver el string es palindromo
 
         //ver si el numero es primo
 
+        if (numero > 1) {
+            for (int i = 2; i <= numero / 2; i++) {
+                if (numero % i == 0) {
+                    primo = false;
+                }else {
+                    primo = true;
+                }
+            }
+        }
+
         //ver cuantas mayusculas tiene la cadena
 
         //si no es primo, factorizalo
+
+        if (primo) {
+            System.out.println(numero + " es un número primo.");
+        } else {
+            System.out.println(numero + " no es un número primo.");
+            System.out.print("Factores de " + numero + ": ");
+            for (int i = 2; i <= numero; i++) {
+                while (numero % i == 0) {
+                    System.out.print(i + " ");
+                    numero /= i;
+                }
+            }
+            System.out.println();
+        }
 
         //mostrar el tamaño de la cadena
 
