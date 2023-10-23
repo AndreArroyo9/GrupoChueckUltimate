82acb4a7617211b1f5b34568f4c9636ae0f336c5
JaimeManero <jmaneromo23de@ikzubirimanteo.com>
2023-10-23 13:05:50 +0200

    Main con el ejercicio de palindromo hecho

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+
 //Ver el string es palindromo
 //ver si el numero es primo
 //ver cuantas mayusculas tiene la cadena
@@ -10,12 +11,37 @@ public class main{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
-        int numero = 0;
-        String cadena = "a";
+        int numero;
+        String cadena = "";
         boolean primo = false;
         System.out.println("Introduce una palabra: ");
         cadena = sc.nextLine();
+
+        System.out.println("Introduce un número: ");
+        numero = sc.nextInt();
+
         //Ver el string es palindromo
+
+        cadena = cadena.replaceAll("\\s", "").toLowerCase();
+
+        int izquierda = 0;
+        int derecha = cadena.length() - 1;
+        boolean esPalindromo = true;
+
+        while (izquierda < derecha) {
+            if (cadena.charAt(izquierda) != cadena.charAt(derecha)) {
+                esPalindromo = false;
+                break;
+            }
+            izquierda++;
+            derecha--;
+        }
+
+        if (esPalindromo) {
+            System.out.println(cadena + " es un palíndromo.");
+        } else {
+            System.out.println(cadena + " no es un palíndromo.");
+        }
 
         //ver si el numero es primo
 
