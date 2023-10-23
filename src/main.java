4f85364d2e59368eebd668bfb5072b9f157fe91c
JaimeManero <jmaneromo23de@ikzubirimanteo.com>
2023-10-23 12:41:36 +0200

    añadir cosas xd

--- src/main.java
+++ src/main.java
@@ -9,6 +9,12 @@ public class main{
 
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
+
+        int numero = 0;
+        String cadena = "a";
+        boolean primo = false;
+        System.out.println("Introduce una palabra: ");
+        String = sc.nextLine;
         //Ver el string es palindromo
 
         //ver si el numero es primo
